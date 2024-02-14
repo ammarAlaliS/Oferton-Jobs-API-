7995d0e9cb05fc2ba8e8eac855b6f5ef796f8141
ammarAlaliS <ammaralialiwa@gmail.com>
2024-02-14 16:08:48 -0600

    create post controller

--- src/main/java/com/AmmarAli/Oferton/job/JobController.java
+++ src/main/java/com/AmmarAli/Oferton/job/JobController.java
@@ -1,7 +1,6 @@
 package com.AmmarAli.Oferton.job;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +11,11 @@ public class JobController {
     @GetMapping("/jobs")
     public List<Job> findAll(){
         return jobs;
+    }
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job){
+        jobs.add(job);
+        return "Job added successfully";
     }
 
 }
