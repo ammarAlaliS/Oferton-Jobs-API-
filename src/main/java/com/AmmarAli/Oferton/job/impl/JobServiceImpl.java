0e09ed123f693192063b54c13bfaa20c35096e3b
ammarAlaliS <ammaralialiwa@gmail.com>
2024-02-14 16:42:58 -0600

    once you have you method declared into your jobServiceImpl you must transfer the logic from you controller method to the serviceImplementation

--- src/main/java/com/AmmarAli/Oferton/job/impl/JobServiceImpl.java
+++ src/main/java/com/AmmarAli/Oferton/job/impl/JobServiceImpl.java
@@ -3,16 +3,18 @@ package com.AmmarAli.Oferton.job.impl;
 import com.AmmarAli.Oferton.job.Job;
 import com.AmmarAli.Oferton.job.JobService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JobServiceImpl implements JobService {
+    private List<Job> jobs = new ArrayList<>();
     @Override
     public List<Job> findAll() {
-        return null;
+        return jobs;
     }
 
     @Override
-    public void createJob() {
-
+    public void createJob(Job job) {
+        jobs.add(job);
     }
 }
