ad8061649f95ea94ddbfc6e8d4b5cb026be8c83e
ammarAlaliS <ammaralialiwa@gmail.com>
2024-02-14 16:35:53 -0600

    in the JobServiceImpl class you must implement JobService interface and into the JobServiceImpl you difine or implement the methods

--- src/main/java/com/AmmarAli/Oferton/job/impl/JobServiceImpl.java
+++ src/main/java/com/AmmarAli/Oferton/job/impl/JobServiceImpl.java
@@ -1,4 +1,18 @@
 package com.AmmarAli.Oferton.job.impl;
 
-public class JobServiceImpl {
+import com.AmmarAli.Oferton.job.Job;
+import com.AmmarAli.Oferton.job.JobService;
+
+import java.util.List;
+
+public class JobServiceImpl implements JobService {
+    @Override
+    public List<Job> findAll() {
+        return null;
+    }
+
+    @Override
+    public void createJob() {
+
+    }
 }
